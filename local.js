46ce0bc1880655b44366596de8e1e29ea0681d2e
sapna0005 <sapna.sahani0005@gmail.com>
2025-01-15 23:55:52 +0530

    added flight booking project

---
+++ local.js
@@ -0,0 +1,141 @@
+let signup=()=>{
+    let signname=document.querySelector("#name").value
+    let signemail=document.querySelector("#email").value
+    let signmobile=document.querySelector("#mobile").value
+    let signpassword=document.querySelector("#pass").value
+
+    let signerrname=document.querySelector("#errname")
+    let signerremail=document.querySelector("#erremail")
+    let signerrmobile=document.querySelector("#errmobile")
+    let signerrpass=document.querySelector("#errpass")
+
+    if (signname==="") {
+        signerrname.innerHTML="Enter your name"
+        signerrname.style.color="red"
+        document.querySelector("#name").placeholder="Enter your name"
+        document.querySelector("#name").focus()
+        return false;
+    }
+    else if (signemail==="") {
+        signerremail.innerHTML="Enter email"
+        signerremail.style.color="red"
+        document.querySelector("#email").placeholder="Enter your email"
+        document.querySelector("#email").focus()
+        return false;
+    }
+    else if (!(signemail.includes('@') && signemail.includes('.com'))) {
+        signerremail.innerHTML="Please enter a valid email"
+        signerremail.style.color="red"
+        document.querySelector("#email").placeholder="Please enter a valid email"
+        document.querySelector("#email").focus()
+        return false;
+    }
+    else if (signmobile==="") {
+        signerrmobile.innerHTML="Please enter mobile number"
+        signerrmobile.style.color="red"
+        document.querySelector("#mobile").placeholder="Please enter mobile number"
+        document.querySelector("#mobile").focus()
+        return false;
+    }
+    else if (signmobile.length!=10) {
+        signerrmobile.innerHTML="Please enter 10-digit number"
+        signerrmobile.style.color="red"
+        document.querySelector("#mobile").placeholder="Please enter 10-digit number"
+        document.querySelector("#mobile").focus()
+        return false;
+    }
+    else if (signpassword==="") {
+        signerrpass.innerHTML="Please enter password"
+        signerrpass.style.color="red"
+        document.querySelector("#pass").placeholder="Please enter password"
+        document.querySelector("#pass").focus()
+        return false;  
+    }
+    else if (!(
+        signpassword.match(/[1234567890]/) &&
+        signpassword.match(/[!@#$%^&*()]/) &&
+        signpassword.match(/[a-z]/) &&
+        signpassword.match(/[A-Z]/)
+      ))
+      {
+        signerrpass.innerHTML="Password should have at least one uppercase letter, one lowercase letter, one digit"
+        signerrpass.style.color="red"
+        document.querySelector("#pass").placeholder="Please enter valid password"
+        document.querySelector("#pass").focus()
+         return false;
+    }
+
+localStorage.setItem("name",signname)
+localStorage.setItem("email",signemail)
+localStorage.setItem("mobile",signmobile)
+localStorage.setItem("pass",signpassword)
+
+location.href="login.html"
+return false;
+}
+
+let login=()=>{
+   let loginname=document.querySelector("#logname").value
+   let loginpass=document.querySelector("#logpass").value
+
+   let localname=localStorage.getItem("name")
+   let localpass=localStorage.getItem("pass")
+  
+   if (loginname==localname && loginpass==localpass) {
+    location.href="home.html";
+   }
+   else
+   {
+    alert("Login Failed")
+   }
+   return false;
+
+}
+
+
+
+var swiper1 = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  
+
+  const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true, // Ensure looping is enabled for continuous scrolling
+    navigation: {
+        nextEl: '.swiper2 .swiper-button-next',
+        prevEl: '.swiper2 .swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper2 .swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+
+// var typed = new Typed('#display', {
+//     strings: ["is Waiting","for you"],
+//     typeSpeed: 80,
+//     loop:true,
+    
+//   });
+
+
+
+  
+  
+
+  
+  
---
+++ vanta.js
@@ -0,0 +1,17 @@
+VANTA.BIRDS({
+    el: "#",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x7dcac2,
+    birdSize: 2.80,
+    wingSpan: 35.00,
+    separation: 100.00,
+    alignment: 43.00,
+    cohesion: 51.00
+    
+  })
